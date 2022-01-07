8e6b03fad9c5c4877d02c03ea8e629997867fb57
Charles <charles94jp@gmail.com>
2022-01-07 23:09:48 +0800

    coding

--- ddns.py
+++ ddns.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+import sys
 import time
 import smtplib
 from email.mime.text import MIMEText
@@ -33,6 +34,9 @@ class DDNS:
                                  'Chrome/96.0.4664.93 Safari/537.36'}
     rrid = ''
     errorCount = 0
+    lastGetCurrentIpError = False
+    lastUpdateDomainIpError = False
+    lastStartError = False
 
     def __init__(self, args, debug=False):
         """
@@ -78,6 +82,7 @@ class DDNS:
 
     def get_current_ip(self):
         """
+        高频使用 api
         update self.currentIp
         :return: None
         """
@@ -88,8 +93,11 @@ class DDNS:
             r = r.split('</title>')[0]
             self.currentIp = r
             self.logger.info("get_current_ip: \tcurrent host ip: " + r)
+            self.lastGetCurrentIpError = False
         else:
             self.logger.error("get_current_ip: \tapi error")
+            self.check_error()
+            self.lastGetCurrentIpError = True
 
     def get_domain_ip(self):
         """
@@ -113,6 +121,7 @@ class DDNS:
 
     def update_domain_ip(self, new_ip):
         """
+        重要 api
         更新域名解析，更新self.domainIp
         :return: None
         """
@@ -126,14 +135,17 @@ class DDNS:
         if r == '300':
             self.domainIp = new_ip
             self.logger.info("update_domain_ip: \tupdate completed: " + self.domainIp)
+            self.lastUpdateDomainIpError = False
         else:
             self.logger.error("update_domain_ip: \tupdate failed")
+            self.check_error()
+            self.lastUpdateDomainIpError = True
 
-    def send_email(self, html_msg):
+    def send_email(self, title, html_msg):
         # 邮件消息，plain是纯文本，html可以自定义样式
         message = MIMEText(html_msg, 'html', 'utf-8')
         # 邮件主题
-        message['Subject'] = 'DDNS服务异常提醒'
+        message['Subject'] = title
         # 发送方信息
         message['From'] = self.mail_user
         # 接受方信息
@@ -155,34 +167,42 @@ class DDNS:
         except smtplib.SMTPException as e:
             ddns.logger.exception(e)
 
+    def check_error(self):
+        if self.lastStartError or self.lastGetCurrentIpError or self.lastUpdateDomainIpError:
+            self.errorCount = self.errorCount + 1
+        else:
+            self.errorCount = 1
+        if self.errorCount > 12:
+            if self.emailAlert:
+                self.send_email('DDNS服务异常提醒',
+                                '<p class="MsoNormal"><span style="font-family:宋体;color:black">您好！</span></p>'
+                                '<p class="MsoNormal" style="text-indent:21.0pt"><span style="font-family:宋体;'
+                                'color:black">您的DDNS服务因异常已停止，这并非是服务本身导致的，可能是ip138.com或NameSilo的api'
+                                '繁忙所致。</span></p><p class="MsoNormal" style="text-indent:21.0pt"><span '
+                                'style="font-family:宋体;color:black">您可以DDNS.log文件查看异常细明，<b>请尽快排查原因'
+                                '并重启DDNS服务</b>，避免IP变动导致您的服务器无法通过域名访问。</span></p><p class="MsoNormal"'
+                                ' style="text-indent:21.0pt"><span style="font-family:宋体;color:black">有任何问题请在'
+                                '<a target="_blank" href="https://github.com/Charles94jp/NameSilo-DDNS">DDNS项目的'
+                                'GitHub Issues</a>中反馈，谢谢您的支持</span></p>')
+            self.logger.error("start: \t连续错误12次，程序退出")
+            exit(-1)
+
+    def test_email(self):
+        self.send_email('DDNS test', '这是<a target="_blank" href="https://github.com/Charles94jp/NameSilo-DDNS">DDNS服务'
+                                     '</a>的测试邮件，收到此邮件代表你的邮件配置无误')
+
     def start(self):
         self.get_domain_ip()
-        last_error = False
         while True:
             try:
                 self.get_current_ip()
                 if self.currentIp != self.domainIp:
                     self.update_domain_ip(self.currentIp)
-                last_error = False
+                self.lastStartError = False
             except BaseException as e:
                 ddns.logger.exception(e)
-                if last_error:
-                    self.errorCount = self.errorCount + 1
-                else:
-                    self.errorCount = 1
-                if self.errorCount > 10:
-                    if self.emailAlert:
-                        self.send_email('<p class="MsoNormal"><span style="font-family:宋体;color:black">您好！</span></p>'
-                                        '<p class="MsoNormal" style="text-indent:21.0pt"><span style="font-family:宋体;'
-                                        'color:black">您的DDNS服务因异常已停止，这并非是服务本身导致的，可能是ip138.com或NameSilo的api'
-                                        '繁忙所致。</span></p><p class="MsoNormal" style="text-indent:21.0pt"><span '
-                                        'style="font-family:宋体;color:black">您可以DDNS.log文件查看异常细明，<b>请尽快排查原因'
-                                        '并重启DDNS服务</b>，避免IP变动导致您的服务器无法通过域名访问。</span></p><p class="MsoNormal"'
-                                        ' style="text-indent:21.0pt"><span style="font-family:宋体;color:black">有任何问题请在'
-                                        '<a target="_blank" href="https://github.com/Charles94jp/NameSoli-DDNS">DDNS项目的'
-                                        'GitHub Issues</a>中反馈，谢谢您的支持</span></p>')
-                    self.logger.error("start: \t连续错误10次，程序退出")
-                    break
+                self.check_error()
+                self.lastStartError = True
             time.sleep(self.frequency)
 
 
@@ -194,6 +214,9 @@ if __name__ == '__main__':
     try:
         with open('conf.json', 'r') as fp:
             ddns = DDNS(json.load(fp))
-        ddns.start()
+        if sys.argv[1] == 'testEmail':
+            ddns.test_email()
+        else:
+            ddns.start()
     except BaseException as e:
         ddns.logger.exception(e)
