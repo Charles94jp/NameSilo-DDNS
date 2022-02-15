f6b78b9ee326beed717411677d8762308dd61691
Charles <charles94jp@gmail.com>
2022-02-15 22:19:01 +0800

    docker

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ LABEL maintainer="https://github.com/Charles94jp/NameSilo-DDNS"
 LABEL description="NameSilo-DDNS"
 
 COPY ddns.py /home/NameSilo-DDNS.back/ddns.py
+COPY docker/ddns-docker /home/NameSilo-DDNS.back/ddns-docker
 COPY conf/ /home/NameSilo-DDNS.back/conf/
 COPY docker/start.sh /start.sh
 
--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 if [ -d "/home/NameSilo-DDNS" ]; then
     cp /home/NameSilo-DDNS.back/ddns.py /home/NameSilo-DDNS/ddns.py
+    cp /home/NameSilo-DDNS.back/ddns-docker /home/NameSilo-DDNS/ddns-docker
+    chmod +x /home/NameSilo-DDNS/ddns-docker
     cp -r /home/NameSilo-DDNS.back/conf /home/NameSilo-DDNS/
     mkdir -p /home/NameSilo-DDNS/log
     cd /home/NameSilo-DDNS
