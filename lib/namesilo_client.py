4f467e4d6544dcb1294ad603b41609a6f9bb5b0c
Resurrection2981 <Jarvisbutler.cc@gmail.com>
2025-12-19 21:50:35 +0800

    修复域名匹配失败

--- lib/namesilo_client.py
+++ lib/namesilo_client.py
@@ -118,7 +118,7 @@ class NameSiloClient:
             _domain = domain['domain'] if domain['host'] == '@' or \
                                           domain['host'] == '' else f"{domain['host']}.{domain['domain']}"
             for record in r:
-                if record.find(f'<host>{_domain}</host>') != -1 and record.find(f'<type>{t}</type>') != -1:
+                if record.find(f'<host>{_domain['host']}</host>') != -1 and record.find(f'<type>{t}</type>') != -1:
                     r = record
                     break
             if type(r) == list:
